f43815697654933821d34057097868c92acff153
locnd-fpt <locnd.fpt@gmail.com>
2023-05-24 16:36:32 +0700

    feature(tetu): create note page; modify header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import LandingPage from './pages/landingpage/LandingPage';
+import Metadata from './components/collapse/Metadata';
+import Notebook from './pages/notepage/Notebook';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/metadata" element={<Metadata />} />
+            <Route path="/note" element={<Notebook />} />
+         </Routes>
+      </BrowserRouter>
+   );
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,14 +9,14 @@ const { Content, Sider } = Layout;
 const { Title, Text } = Typography;
 
 const navItems = [
-   { title: 'Read Document', key: '1', icon: <FileTwoTone className='nav_icon' /> },
-   { title: 'New Session', key: '2', icon: <FolderAddTwoTone className='nav_icon' /> },
-   { title: 'New Notebook', key: '3', icon: <BookTwoTone className='nav_icon' /> },
-   { title: 'New Canvas', key: '4', icon: <AppstoreTwoTone className='nav_icon' /> },
-   { title: 'Graph View', key: '5', icon: <StarTwoTone className='nav_icon' /> },
-   { title: 'Calendar View', key: '6', icon: <CalendarTwoTone className='nav_icon' /> },
-   { title: 'Citation Box', key: '7', icon: <WalletTwoTone className='nav_icon' /> },
-   { title: 'Revision', key: '8', icon: <SecurityScanTwoTone className='nav_icon' /> },
+   { title: 'Read Document', key: '1', icon: <FileTwoTone className='nav_icon' />, navLink: '' },
+   { title: 'New Session', key: '2', icon: <FolderAddTwoTone className='nav_icon' />, navLink: '' },
+   { title: 'New Notebook', key: '3', icon: <BookTwoTone className='nav_icon' />, navLink: '/note' },
+   { title: 'New Canvas', key: '4', icon: <AppstoreTwoTone className='nav_icon' />, navLink: '' },
+   { title: 'Graph View', key: '5', icon: <StarTwoTone className='nav_icon' />, navLink: '' },
+   { title: 'Calendar View', key: '6', icon: <CalendarTwoTone className='nav_icon' />, navLink: '' },
+   { title: 'Citation Box', key: '7', icon: <WalletTwoTone className='nav_icon' />, navLink: '' },
+   { title: 'Revision', key: '8', icon: <SecurityScanTwoTone className='nav_icon' />, navLink: '' },
 ];
 
 const Home = () => {
@@ -51,10 +51,10 @@ const Home = () => {
                         <Row gutter={[30, 30]}>
                            {navItems.map((item, index) => (
                               <Col key={index} className="gutter-row" span={6}>
-                                 <button className='nav_btn' >
+                                 <a href={item.navLink} className='nav_btn' >
                                     {item.icon}
                                     <Text style={{ fontSize: '20px' }} strong>{item.title}</Text>
-                                 </button>
+                                 </a>
                               </Col>
                            ))}
                         </Row>
