de9d9825af334b255db36a3bef81f3d84393f5ef
tuandq39 <tuandqse160189@fpt.edu.vn>
2023-05-30 07:59:17 +0700

    add payment

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import LandingPage from './pages/landingpage/LandingPage';
 import Metadata from './components/collapse/Metadata';
 import Notebook from './pages/notepage/Notebook';
 import FilePage from './pages/file/FilePage';
+import PricingPage from './pages/pricing/PricingPage';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 function App() {
    return (
-      <BrowserRouter>
+     <PayPalScriptProvider options={{"client-id" :"AYoTMzxCm2u7ot7z8kCWQGMgUlf79LmiJIrLY2zBpIemVUhBZh3nhF4llvBr5bIAk4jNgey06AAae5ex"}}>
+       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/metadata" element={<Metadata />} />
-            <Route path="/note" element={<Notebook />} />
-            <Route path="/file" element={<FilePage />} />
+           <Route path="/" element={<LandingPage />} />
+           <Route path="/home" element={<Home />} />
+           <Route path="/metadata" element={<Metadata />} />
+           <Route path="/note" element={<Notebook />} />
+           <Route path="/file" element={<FilePage />} />
+           <Route path="/pricing" element={<PricingPage />} />
          </Routes>
-      </BrowserRouter>
+       </BrowserRouter>
+     </PayPalScriptProvider>
    );
 }
 
