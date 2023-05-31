f3610eb0b783c1c5f4d41d3c38830c2e8581af8f
locnd-fpt <locnd.fpt@gmail.com>
2023-05-31 22:39:28 +0700

    chore(tetu): add env

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,49 +1,89 @@
 import React from 'react';
-import {Dropdown, Menu} from "antd";
-import {BookOutlined, EllipsisOutlined, FilePdfOutlined, FolderOutlined} from "@ant-design/icons";
-import {FileType} from "../../enums/FileType";
+import { Dropdown, Menu, message } from "antd";
+import { BookOutlined, EllipsisOutlined, FilePdfOutlined, FolderOutlined, FileAddOutlined, CloseCircleOutlined, FormOutlined } from "@ant-design/icons";
+import { FileType } from "../../enums/FileType";
+import { deleteDocument } from '../../services/firebase';
 
-const TeTuMenu = ({folderData}) => {
+const TeTuMenu = ({ folderData }) => {
     console.log(folderData)
-    const deleteFolder = () => {
-
+    const deleteFolder = async ({ fid }) => {
+        await deleteDocument("folders", fid);
+        message.success("Delete folder successfully!");
     };
-    const deleteNote = () => {
 
+    const deleteNote = async ({ nid }) => {
+        await deleteDocument("notes", nid);
+        message.success("Delete note successfully!");
     }
 
     const deleteFile = () => {
 
     }
+
+    const handleMenuClick = () => {
+    };
+
+    const renameFolder = (folderId) => {
+
+    };
+
     const DropDown = (fileType) => {
         const isFolder = fileType.fileType === FileType.Folder;
         console.log(isFolder)
         return (<Dropdown
-                overlay={<Menu>
+            overlay={
+                <Menu>
                     {isFolder ? <Menu.Item key="createNewPage">Create Page</Menu.Item> : <></>}
-                    <Menu.Item key="deleteFolder">Delete Folder</Menu.Item>
-                </Menu>}
-                trigger={['click']}
-            >
-          <span style={{float: 'right'}}>
-            <EllipsisOutlined/>
-          </span>
-            </Dropdown>)
-    }
-    return (<Menu
-            mode="inline"
-            theme="light"
+                    <Menu.Item key="renameFolder"><FormOutlined /> Rename</Menu.Item>
+                    <Menu.Item key="deleteFolder"><CloseCircleOutlined /> Delete</Menu.Item>
+                </Menu>
+            }
+            trigger={['click']}
         >
-            <Menu.SubMenu key={folderData.id} title={folderData.folder_name} icon={<FolderOutlined/>}
-                          popupOffset={[20, 20]}>
-                {folderData.notes.map((note) => (<Menu.Item key={note} icon={<BookOutlined/>}>{note}
-                        <DropDown fileType={FileType.Note.toString()}/>
-                    </Menu.Item>))}
-                {folderData.files.map((file) => (<Menu.Item key={file} icon={<FilePdfOutlined/>}>{file}
-                        <DropDown fileType={FileType.Pdf.toString()}/>
-                    </Menu.Item>))}
+            <span style={{ float: 'right' }}>
+                <EllipsisOutlined />
+            </span>
+        </Dropdown>)
+    }
+    return (
+        <Dropdown
+            overlay={
+                <Menu>
+                    <Menu.Item key="createNewPage"><FileAddOutlined /> Create Page</Menu.Item>
+                    <Menu.Item key="renameFolder"><FormOutlined /> Rename</Menu.Item>
+                    <Menu.Item
+                        key="deleteFolder"
+                        onClick={() => deleteFolder({ fid: folderData.id })}>
+                        <CloseCircleOutlined /> Delete
+                    </Menu.Item>
+                </Menu>
+            }
+            trigger={['contextMenu']}
+        >
+
+            <Menu.SubMenu
+                key={folderData.id}
+                title={folderData.folder_name}
+                icon={<FolderOutlined />}
+            // popupOffset={[10, 10]}
+            // style={{marginLeft: 10}}
+            // style={{ height: 30 }}
+            >
+                {folderData.notes.map((note) => (
+                    <Menu.Item key={note} icon={<BookOutlined />} style={{ height: 30 }} >
+                        {note}
+                        <DropDown fileType={FileType.Note.toString()} />
+                    </Menu.Item>))
+                }
+                {folderData.files.map((file) => (
+                    <Menu.Item key={file} icon={<FilePdfOutlined />} style={{ height: 30 }}>
+                        {file}
+                        <DropDown fileType={FileType.Pdf.toString()} />
+                    </Menu.Item>))
+                }
             </Menu.SubMenu>
-        </Menu>);
+        </Dropdown>
+    );
 };
 
 export default TeTuMenu;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,14 +6,14 @@ import { getFirestore, collection, query, where, getDocs, addDoc } from "firebas
 
 // Initialize Firebase
 const firebaseConfig = {
-   // Your Firebase configuration
-   apiKey: "AIzaSyDP9EHsdddH7fjGgmo-r0Vz6KdkpzFRYqs",
-   authDomain: "tetu-3be72.firebaseapp.com",
-   projectId: "tetu-3be72",
-   storageBucket: "tetu-3be72.appspot.com",
-   messagingSenderId: "827087020606",
-   appId: "1:827087020606:web:117bb5069edd78362a276f",
-   measurementId: "G-BSY6RBRE8M",
+    // Your Firebase configuration
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,78 +25,78 @@ const db = getFirestore(app);
 
 // Create a document in a Firestore collection
 export const createDocument = async (collections, data) => {
-   try {
-      await addDoc(collection(db, collections), data);
-   } catch (error) {
-      console.error('Error creating document:', error);
-      throw error;
-   }
+    try {
+        await addDoc(collection(db, collections), data);
+    } catch (error) {
+        console.error('Error creating document:', error);
+        throw error;
+    }
 };
 
 // Read a document from a Firestore collection
 export const getDocument = async (collection, id) => {
-   try {
-      const docRef = await db.collection(collection).doc(id).get();
-      if (docRef.exists) {
-         return { id: docRef.id, ...docRef.data() };
-      } else {
-         throw new Error('Document not found.');
-      }
-   } catch (error) {
-      console.error('Error getting document:', error);
-      throw error;
-   }
+    try {
+        const docRef = await db.collection(collection).doc(id).get();
+        if (docRef.exists) {
+            return { id: docRef.id, ...docRef.data() };
+        } else {
+            throw new Error('Document not found.');
+        }
+    } catch (error) {
+        console.error('Error getting document:', error);
+        throw error;
+    }
 };
 
 // Update a document in a Firestore collection
 export const updateDocument = async (collection, id, data) => {
-   try {
-      await db.collection(collection).doc(id).update(data);
-   } catch (error) {
-      console.error('Error updating document:', error);
-      throw error;
-   }
+    try {
+        await db.collection(collection).doc(id).update(data);
+    } catch (error) {
+        console.error('Error updating document:', error);
+        throw error;
+    }
 };
 
 // Delete a document from a Firestore collection
 export const deleteDocument = async (collection, id) => {
-   try {
-      await db.collection(collection).doc(id).delete();
-   } catch (error) {
-      console.error('Error deleting document:', error);
-      throw error;
-   }
+    try {
+        await db.collection(collection).doc(id).delete();
+    } catch (error) {
+        console.error('Error deleting document:', error);
+        throw error;
+    }
 };
 
 // Query multiple documents from a collection
 export const getDocuments = async (collection) => {
-   try {
-      const querySnapshot = await db.collection(collection).get();
-      const documents = [];
-      querySnapshot.forEach((doc) => {
-         documents.push({ id: doc.id, ...doc.data() });
-      });
-      return documents;
-   } catch (error) {
-      console.error('Error getting documents:', error);
-      throw error;
-   }
+    try {
+        const querySnapshot = await db.collection(collection).get();
+        const documents = [];
+        querySnapshot.forEach((doc) => {
+            documents.push({ id: doc.id, ...doc.data() });
+        });
+        return documents;
+    } catch (error) {
+        console.error('Error getting documents:', error);
+        throw error;
+    }
 };
 
 // Query multiple documents from a collection with condition
 export const queryDocuments = async (collections, field, operator, value) => {
-   try {
-      const documents = [];
-      const q = query(collection(db, collections), where(field, operator, value));
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-         documents.push({ id: doc.id, ...doc.data() });
-      });
-      return documents;
-   } catch (error) {
-      console.error('Error querying documents:', error);
-      throw error;
-   }
+    try {
+        const documents = [];
+        const q = query(collection(db, collections), where(field, operator, value));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            documents.push({ id: doc.id, ...doc.data() });
+        });
+        return documents;
+    } catch (error) {
+        console.error('Error querying documents:', error);
+        throw error;
+    }
 };
 
 
@@ -106,50 +106,50 @@ export const queryDocuments = async (collections, field, operator, value) => {
 const storage = getStorage();
 
 export const uploadFile = (file, setFileUrl) => {
-   try {
-      const storageRef = ref(storage, `/files/${file.name}`);
-      const uploadTask = uploadBytesResumable(storageRef, file);
+    try {
+        const storageRef = ref(storage, `/files/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on(
-         "state_changed",
-         (snapshot) => {
-            console.log("upload file successfully");
-         },
-         (err) => console.log(err),
-         () => {
-            getDownloadURL(uploadTask.snapshot.ref).then(url => {
-               console.log(url);
-               setFileUrl(url);
-            })
-         }
-      );
-      // return fileUrl;
-   } catch (error) {
-      console.error('Error uploading file:', error);
-      throw error;
-   }
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                console.log("upload file successfully");
+            },
+            (err) => console.log(err),
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then(url => {
+                    console.log(url);
+                    setFileUrl(url);
+                })
+            }
+        );
+        // return fileUrl;
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        throw error;
+    }
 };
 
 // Delete a file from Firebase storage
 export const deleteFile = async (fileURL) => {
-   try {
-      const fileRef = storage.refFromURL(fileURL);
-      await fileRef.delete();
-   } catch (error) {
-      console.error('Error deleting file:', error);
-      throw error;
-   }
+    try {
+        const fileRef = storage.refFromURL(fileURL);
+        await fileRef.delete();
+    } catch (error) {
+        console.error('Error deleting file:', error);
+        throw error;
+    }
 };
 
 // Get download URL
 export const getFileDownloadURL = async (fileURL) => {
-   try {
-      const fileRef = storage.refFromURL(fileURL);
-      return fileRef.getDownloadURL();
-   } catch (error) {
-      console.error('Error getting file download URL:', error);
-      throw error;
-   }
+    try {
+        const fileRef = storage.refFromURL(fileURL);
+        return fileRef.getDownloadURL();
+    } catch (error) {
+        console.error('Error getting file download URL:', error);
+        throw error;
+    }
 };
 
 
