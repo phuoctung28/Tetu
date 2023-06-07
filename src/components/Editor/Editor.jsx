3a2d8a5e03463a629a0beab80b28f04edcb0e211
locnd-fpt <locnd.fpt@gmail.com>
2023-06-07 12:42:15 +0700

    feature(tetu): add editorjs

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,11 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactQuill from 'react-quill';
+import EditorJS from "@editorjs/editorjs";
+import Header from '@editorjs/header';
+import Table from '@editorjs/table'
+import List from '@editorjs/list'
+import LinkTool from '@editorjs/link'
+import Image from '@editorjs/image'
+import Quote from '@editorjs/quote'
+import CheckList from '@editorjs/checklist'
 import 'react-quill/dist/quill.snow.css';
 import './Editor.css';
 
+const DEFAULT_INITIAL_DATA = {
+    "time": new Date().getTime(),
+    "blocks": [
+        {
+            "type": "header",
+            "data": {
+                "text": "This is my awesome editor!",
+                "level": 1
+            }
+        },
+    ]
+}
+
 const TetuEditor = ({ editorData, setNoteContent }) => {
     const [editorValue, setEditorValue] = useState('');
+    const ejInstance = useRef();
 
+    const initEditor = () => {
+        const editor = new EditorJS({
+            holder: 'editorjs',
+            onReady: () => {
+                ejInstance.current = editor;
+            },
+            autofocus: true,
+            data: DEFAULT_INITIAL_DATA,
+            onChange: async () => {
+                let content = await editor.saver.save();
+                console.log(content);
+            },
+            tools: {
+                header: Header,
+                table: Table,
+                list: List,
+                linkTool: LinkTool,
+                image: Image,
+                quote: Quote,
+                checklist: CheckList,
+                paragraph: {
+                    config: {
+                        placeholder: 'Tell your story...'
+                    }
+                }
+            },
+        });
+    };
+    useEffect(() => {
+        if (ejInstance.current === null) {
+            initEditor();
+        }
+
+        return () => {
+            ejInstance?.current?.destroy();
+            ejInstance.current = null;
+        };
+    }, []);
     useEffect(() => {
         setEditorValue(editorData);
     }, [editorData]);
@@ -17,6 +77,7 @@ const TetuEditor = ({ editorData, setNoteContent }) => {
 
     return (
         <div className="editor-container">
+            {/* <div id='editorjs'></div> */}
             <ReactQuill
                 value={editorValue}
                 onChange={handleChange}
