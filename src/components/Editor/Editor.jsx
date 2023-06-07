43af37c566c99d4a5e3741c34b3323d7927a8106
locnd-fpt <locnd.fpt@gmail.com>
2023-06-07 23:28:17 +0700

    feature(tetu): intergrate editorjs

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -4,10 +4,14 @@ import EditorJS from "@editorjs/editorjs";
 import Header from '@editorjs/header';
 import Table from '@editorjs/table'
 import List from '@editorjs/list'
-import LinkTool from '@editorjs/link'
-import Image from '@editorjs/image'
-import Quote from '@editorjs/quote'
+import Code from '@editorjs/code'
+import Marker from '@editorjs/marker'
 import CheckList from '@editorjs/checklist'
+import InlineCode from '@editorjs/inline-code'
+import DragDrop from 'editorjs-drag-drop';
+import Undo from 'editorjs-undo';
+import Paragraph from "@editorjs/paragraph";
+import Underline from '@editorjs/underline';
 import 'react-quill/dist/quill.snow.css';
 import './Editor.css';
 
@@ -26,46 +30,65 @@ const DEFAULT_INITIAL_DATA = {
 
 const TetuEditor = ({ editorData, setNoteContent }) => {
     const [editorValue, setEditorValue] = useState('');
-    const ejInstance = useRef();
+    // const ejInstance = useRef();
+    // const initEditor = () => {
+    //     const editor = new EditorJS({
+    //         holder: 'editorjs',
+    //         onReady: () => {
+    //             ejInstance.current = editor;
+    //             // new Undo({ editor });
+    //             new DragDrop(editor);
+    //         },
+    //         autofocus: true,
+    //         data: DEFAULT_INITIAL_DATA,
+    //         onChange: async () => {
+    //             let content = await editor.saver.save();
+    //             console.log(content);
+    //         },
+    //         tools: {
+    //             header: {
+    //                 class: Header,
+    //                 inlineToolbar: ['link'],
+    //                 shortcut: 'CTRL+SHIFT+H',
+    //             },
+    //             table: Table,
+    //             underline: {
+    //                 class: Underline,
+    //                 shortcut: 'CTRL+U',
+    //             },
+    //             marker: {
+    //                 class: Marker,
+    //                 shortcut: 'CTRL+M',
+    //             },
+    //             inlineCode: {
+    //                 class: InlineCode,
+    //                 shortcut: 'CTRL+E'
+    //             },
+    //             list: List,
+    //             code: Code,
+    //             image: Image,
+    //             checklist: CheckList,
+    //             paragraph: {
+    //                 class: Paragraph,
+    //                 inlineToolbar: true,
+    //                 config: {
+    //                     placeholder: 'Press "TAB" for commands...'
+    //                 }
+    //             },
+    //         },
+    //     });
+    // };
+    // useEffect(() => {
+    //     if (ejInstance.current === null) {
+    //         initEditor();
+    //     }
 
-    const initEditor = () => {
-        const editor = new EditorJS({
-            holder: 'editorjs',
-            onReady: () => {
-                ejInstance.current = editor;
-            },
-            autofocus: true,
-            data: DEFAULT_INITIAL_DATA,
-            onChange: async () => {
-                let content = await editor.saver.save();
-                console.log(content);
-            },
-            tools: {
-                header: Header,
-                table: Table,
-                list: List,
-                linkTool: LinkTool,
-                image: Image,
-                quote: Quote,
-                checklist: CheckList,
-                paragraph: {
-                    config: {
-                        placeholder: 'Tell your story...'
-                    }
-                }
-            },
-        });
-    };
-    useEffect(() => {
-        if (ejInstance.current === null) {
-            initEditor();
-        }
+    //     return () => {
+    //         ejInstance?.current?.destroy();
+    //         ejInstance.current = null;
+    //     };
+    // }, []);
 
-        return () => {
-            ejInstance?.current?.destroy();
-            ejInstance.current = null;
-        };
-    }, []);
     useEffect(() => {
         setEditorValue(editorData);
     }, [editorData]);
@@ -77,7 +100,7 @@ const TetuEditor = ({ editorData, setNoteContent }) => {
 
     return (
         <div className="editor-container">
-            {/* <div id='editorjs'></div> */}
+            {/* <div holder='editorjs' id='editorjs'></div> */}
             <ReactQuill
                 value={editorValue}
                 onChange={handleChange}
@@ -87,7 +110,7 @@ const TetuEditor = ({ editorData, setNoteContent }) => {
                         [{ header: [1, 2, false] }],
                         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
                         [{ list: 'ordered' }, { list: 'bullet' }],
-                        ['image'],
+                        // ['image'],
                     ],
                 }}
                 formats={[
@@ -100,7 +123,7 @@ const TetuEditor = ({ editorData, setNoteContent }) => {
                     'list',
                     'bullet',
                     'link',
-                    'image',
+                    // 'image',
                 ]}
             />
         </div>
