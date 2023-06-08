322a6d4c9ee1fb6904d5786fdc174687455e14ab
locnd-fpt <locnd.fpt@gmail.com>
2023-06-08 23:04:51 +0700

    chore(tetu): fix wrapper tag in pricing plan

--- src/pages/pricing/PricingPlan.jsx
+++ src/pages/pricing/PricingPlan.jsx
@@ -14,39 +14,73 @@ const PricingPlan = () => {
         setIsModalOpen(false);
     };
     return (
-        <div className="pricing-plan-container">
-            <div className="pricing-content">
-                <div className="pricing-plan-basic">
-                    <div className="plan-heading">
-                        <p className="plan-title">Basic</p>
-                        <p className="price-text">0đ<span class="text-small"> / month</span></p>
+        <div>
+            <div className="pricing-plan-container">
+                <div className="pricing-content">
+                    <div className="pricing-plan-basic">
+                        <div className="plan-heading">
+                            <p className="plan-title">Basic</p>
+                            <p className="price-text">0đ<span class="text-small"> / month</span></p>
+                        </div>
+                        <ul className="benefits">
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Read file</p>
+                            </li>
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Take & manage note</p>
+                            </li>
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Table view </p>
+                            </li>
+                            <li className="options basic">
+                                <img src={cross} alt="cross" />
+                                <p>Canvas note</p>
+                            </li>
+                            <li className="options basic">
+                                <img src={cross} alt="cross" />
+                                <p>Graph view</p>
+                            </li>
+                        </ul>
+                        <div className="btn-select-plan">
+                            Current plan
+                        </div>
                     </div>
-                    <ul className="benefits">
-                        <li className="options">
-                            <img src={check} alt="check" />
-                            <p>Read file</p>
-                        </li>
-                        <li className="options">
-                            <img src={check} alt="check" />
-                            <p>Take & manage note</p>
-                        </li>
-                        <li className="options">
-                            <img src={check} alt="check" />
-                            <p>Table view </p>
-                        </li>
-                        <li className="options basic">
-                            <img src={cross} alt="cross" />
-                            <p>Canvas note</p>
-                        </li>
-                        <li className="options basic">
-                            <img src={cross} alt="cross" />
-                            <p>Graph view</p>
-                        </li>
-                    </ul>
-                    <div className="btn-select-plan">
-                        Current plan
+                    <div className="pricing-plan-premium">
+                        <div className="plan-heading">
+                            <p className="plan-title">Premium</p>
+                            <p className="price-text">17.000đ<span class="text-small"> / month</span></p>
+                        </div>
+                        <ul className="benefits">
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Read file</p>
+                            </li>
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Take & manage note</p>
+                            </li>
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Table view </p>
+                            </li>
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Canvas note</p>
+                            </li>
+                            <li className="options">
+                                <img src={check} alt="check" />
+                                <p>Graph view</p>
+                            </li>
+                        </ul>
+                        <div className="btn-select-plan premium" onClick={showModal}>
+                            Choose this plan
+                        </div>
                     </div>
                 </div>
+
                 <div className="pricing-plan-premium">
                     <div className="plan-heading">
                         <p className="plan-title">Premium</p>
@@ -79,49 +113,16 @@ const PricingPlan = () => {
                     </div>
                 </div>
             </div>
-
-            <div className="pricing-plan-premium">
-               <div className="plan-heading">
-                  <p className="plan-title">Premium</p>
-                  <p className="price-text">17.000đ<span class="text-small"> / month</span></p>
-               </div>
-               <ul className="benefits">
-                  <li className="options">
-                     <img src={check} alt="check" />
-                     <p>Read file</p>
-                  </li>
-                  <li className="options">
-                     <img src={check} alt="check" />
-                     <p>Take & manage note</p>
-                  </li>
-                  <li className="options">
-                     <img src={check} alt="check" />
-                     <p>Table view </p>
-                  </li>
-                  <li className="options">
-                     <img src={check} alt="check" />
-                     <p>Canvas note</p>
-                  </li>
-                  <li className="options">
-                     <img src={check} alt="check" />
-                     <p>Graph view</p>
-                  </li>
-               </ul>
-               <div className="btn-select-plan premium" onClick={showModal}>
-                  Choose this plan
-               </div>
-            </div>
-         </div>
-         <Modal
-            title="Scan this to finish payment"
-            open={isModalOpen}
-            onCancel={handleCancel}
-            footer={null}
-         >
-            <img src={qrPayment} alt={"payment"} className='qrPayment' />
-         </Modal>
-      </div>
-   )
+            <Modal
+                title="Scan this to finish payment"
+                open={isModalOpen}
+                onCancel={handleCancel}
+                footer={null}
+            >
+                <img src={qrPayment} alt={"payment"} className='qrPayment' />
+            </Modal>
+        </div>
+    )
 
 }
 
